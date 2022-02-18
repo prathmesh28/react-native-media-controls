6f07e54515e5d1c7681f06bfde52ac69cb515b80
PrathameshTI <prathamesh@teaminertia.com>
2022-02-18 11:54:06 +0530

    play butt custom style

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,27 +1,35 @@
 import React from "react";
-import { TouchableOpacity, View, ActivityIndicator, Image } from "react-native";
+import { TouchableOpacity, View, ActivityIndicator, Image, ViewStyle } from "react-native";
 import styles from "./MediaControls.style";
 import { getPlayerStateIcon } from "./utils";
 import { Props } from "./MediaControls";
 import { PLAYER_STATES } from "./constants/playerStates";
+
+export type CustomSliderStyle = {
+  playButtonStyle: ViewStyle;
+};
 
 type ControlsProps = Pick<
   Props,
   "isLoading" | "mainColor" | "playerState" | "onReplay"
 > & {
   onPause: () => void;
+  customSliderStyle?: CustomSliderStyle;
 };
 
 const Controls = (props: ControlsProps) => {
-  const { isLoading, playerState, onReplay, onPause } = props;
+  const { customSliderStyle, isLoading, playerState, onReplay, onPause } = props;
   const icon = getPlayerStateIcon(playerState);
   const pressAction = playerState === PLAYER_STATES.ENDED ? onReplay : onPause;
+ 
+  
+  const customPlayButtonStyle = customSliderStyle?.playButtonStyle || {};
 
   const content = isLoading ? (
     <ActivityIndicator size="large" color="#FFF" />
   ) : (
     <TouchableOpacity
-      style={[styles.playButton]}
+      style={[styles.playButton,customPlayButtonStyle]}
       onPress={pressAction}
       accessibilityLabel={PLAYER_STATES.PAUSED ? "Tap to Play" : "Tap to Pause"}
       accessibilityHint={"Plays and Pauses the Video"}
